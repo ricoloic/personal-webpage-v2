5fbba6ea8edfe6adbb9b2e9f13c161d6d3034f91
ricoloic <lorico@ebay.com>
2023-07-18 21:46:58 -0400

    chore: added vercel analitics

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
+import { inject } from '@vercel/analytics';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './main.css';
+
+inject();
 
 const container: HTMLElement = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
