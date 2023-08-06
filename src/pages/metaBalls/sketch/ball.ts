41cdf48ecdabf061471eb5c29609b735fe36986b
lorico <ricoloic4@gmail.com>
2023-08-06 13:13:03 -0400

    feat: added meta-balls references and linear-interpolation

--- src/pages/metaBalls/sketch/ball.ts
+++ src/pages/metaBalls/sketch/ball.ts
@@ -9,29 +9,26 @@ export class Ball {
 
   public radius: number;
 
-  private scale: number;
-
   constructor(p5: P5, scale: number) {
     this.p5 = p5;
-    this.scale = scale;
-    this.radius = p5.random(60, 100);
+    this.radius = p5.random(60, 120);
     this.pos = p5.createVector(
-      p5.random(this.radius, p5.width - this.radius),
-      p5.random(this.radius, p5.height - this.radius)
+      p5.random(this.radius + scale, p5.width - this.radius - scale),
+      p5.random(this.radius + scale, p5.height - this.radius - scale)
     );
     this.vel = P5.Vector.random2D().mult(p5.random(2, 5));
   }
 
   checkEdges() {
     if (
-      this.pos.x - this.radius - this.scale < 0 ||
-      this.pos.x + this.radius + this.scale > this.p5.width
+      this.pos.x - this.radius < 0 ||
+      this.pos.x + this.radius > this.p5.width
     ) {
       this.vel.x *= -1;
     }
     if (
-      this.pos.y - this.radius - this.scale <= 0 ||
-      this.pos.y + this.radius + this.scale >= this.p5.height
+      this.pos.y - this.radius <= 0 ||
+      this.pos.y + this.radius >= this.p5.height
     ) {
       this.vel.y *= -1;
     }
